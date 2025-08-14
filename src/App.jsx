231766690628da6b0a2da4f61d8c21e3cad994c4
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-14 14:11:52 +0200

    nutrition added, desktop layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Instructions from "./components/Instructions";
 import Nutrition from "./components/Nutrition";
 function App() {
   return (
-    <main className="w-screen h-screen flex flex-col bg-custom-white">
-      <img src="/image-omelette.jpeg" alt="" />
-      <div className="flex flex-col gap-8 px-8 py-10 font-outfit ">
-        <Intro />
-        <Ingredients />
-        <Instructions />
-        <Nutrition />
-      </div>
-    </main>
+    <div className="flex flex-col md:items-center md:justify-center md:min-h-screen md:p-10 bg-custom-brown-800">
+      <main className="w-full flex flex-col bg-custom-white md:max-w-2xl md:rounded-2xl">
+        <img
+          className="md:m-8 md:rounded-xl"
+          src="/image-omelette.jpeg"
+          alt="Image of an Omelette"
+        />
+        <div className="flex flex-col gap-8 px-8 py-10 font-outfit bg-custom-white md:rounded-b-2xl">
+          <Intro />
+          <Ingredients />
+          <Instructions />
+          <Nutrition />
+        </div>
+      </main>
+    </div>
   );
 }
 
--- src/components/Nutrition.jsx
+++ src/components/Nutrition.jsx
@@ -1,8 +1,12 @@
 import { nutritionData } from "../data/data";
 export default function Nutrition() {
   const nutritionElements = nutritionData.map((item, index) => (
-    <li className="border-b-1 border-custom-stone-150" key={index}>
-      {item.name} <span>{item.amount}</span>
+    <li
+      className="border-b-1 border-custom-stone-150 last-of-type:border-none grid grid-cols-2 py-3 first-of-type:pt-0 pl-6"
+      key={index}
+    >
+      {item.name}{" "}
+      <span className="text-custom-brown-800 font-bold">{item.amount}</span>
     </li>
   ));
 
@@ -11,6 +15,10 @@ export default function Nutrition() {
       <h2 className="font-young-serif text-custom-brown-800 text-3xl">
         Nutrition
       </h2>
+      <p className="text-custom-stone-600 font-light">
+        The table below shows nutritional values per serving without the
+        additional fillings.
+      </p>
       <ul>{nutritionElements}</ul>
     </section>
   );
